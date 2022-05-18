0f4e8a5cb78982715d996d02954f4b0e0036b5aa
Piyush Chawla <piyushcha@gmail.com>
2022-05-17 20:37:19 -0400

    Create api.py

---
+++ api.py
@@ -0,0 +1,35 @@
+import flask
+from flask import request, jsonify
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+# Create some test data for our catalog in the form of a list of dictionaries.
+storagevendor = [
+    {'id': 0,
+     'Vendor': 'DellEMC',
+     'Product': 'VMAX'
+     },
+    {'id': 1,
+     'Vendor': 'DellEMC',
+     'Product': 'Isilon'
+    },
+    {'id': 2,
+     'Vendor': 'NetApp',
+     'Product': 'FAS2012'
+    }
+]
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return '''<h1>Reading Archive</h1>
+<p>A prototype API for Storage Vendor.</p>'''
+
+
+# A route to return all of the available entries in our catalog.
+@app.route('/api/v1/resources/storagevendor/all', methods=['GET'])
+def api_all():
+    return jsonify(storagevendor)
+
+app.run()
