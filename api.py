5e926117a59604e120c552dea38321ab2d8cf2f2
Piyush Chawla <piyushcha@gmail.com>
2022-05-17 21:08:56 -0400

    Update api.py

--- api.py
+++ api.py
@@ -32,4 +32,27 @@ def home():
 def api_all():
     return jsonify(storagevendor)
 
+@app.route('/api/v1/resources/storagevendor', methods=['GET'])
+def api_id():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if 'id' in request.args:
+        id = int(request.args['id'])
+    else:
+        return "Error: No id field provided. Please specify an id."
+
+    # Create an empty list for our results
+    results = []
+
+    # Loop through the data and match results that fit the requested ID.
+    # IDs are unique, but other fields might return many results
+    for i in storagevendor:
+        if i['id'] == id:
+            results.append(i)
+
+    # Use the jsonify function from Flask to convert our list of
+    # Python dictionaries to the JSON format.
+    return jsonify(results)
+
 app.run()
