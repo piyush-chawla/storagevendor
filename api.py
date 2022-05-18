27caedd33a22849506b12a95a2eda8253cf9b06e
Piyush Chawla <piyushcha@gmail.com>
2022-05-17 21:55:46 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,8 +1,11 @@
 import flask
 from flask import request, jsonify
+from flask_httpauth import HTTPBasicAuth
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+
+auth = HTTPBasicAuth()
 
 # Create some test data for our catalog in the form of a list of dictionaries.
 storagevendor = [
@@ -26,13 +29,27 @@ def home():
     return '''<h1>Storage Vendor List</h1>
 <p>A prototype API for Storage Vendor.</p>'''
 
+@auth.login_required
+def get_response():
+	return jsonify('You are an authenticate person to see this message')
+	
+@auth.verify_password
+def authenticate(username, password):
+	if username and password:
+		if username == 'piyush' and password == 'pc1611':
+			return True
+		else:
+			return False
+	return False
 
 # A route to return all of the available entries in our catalog.
 @app.route('/api/v1/resources/storagevendor/all', methods=['GET'])
+@auth.login_required
 def api_all():
     return jsonify(storagevendor)
 
 @app.route('/api/v1/resources/storagevendor', methods=['GET'])
+@auth.login_required
 def api_id():
     # Check if an ID was provided as part of the URL.
     # If ID is provided, assign it to a variable.
